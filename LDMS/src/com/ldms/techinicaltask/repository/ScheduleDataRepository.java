4c443e7bdd90617268af0d49acd2dcb54f000dca
bensonlihker <benson.li198508@gmail.com>
2021-09-21 17:25:50 +0100

    Revert "first version"
    This reverts commit ae1fa21076c61ac259db540bd968c56b0d2e71d4.

--- LDMS/src/com/ldms/techinicaltask/repository/ScheduleDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ldms.techinicaltask.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ldms.techinicaltask.entity.ScheduleDataEntity;
-
-public interface ScheduleDataRepository extends JpaRepository<ScheduleDataEntity, Integer> {
-
-}
